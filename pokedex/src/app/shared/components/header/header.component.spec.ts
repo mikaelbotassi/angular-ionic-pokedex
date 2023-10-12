8e1f3f925bffa58e4376b9307d949812abf65199
Mikael <elkamilive@gmail.com>
2023-10-12 14:19:12 -0300

    test: new header tests working

--- pokedex/src/app/shared/components/header/header.component.spec.ts
+++ pokedex/src/app/shared/components/header/header.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { HeaderComponent } from './header.component';
+import { By } from '@angular/platform-browser';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -18,7 +19,19 @@ describe('HeaderComponent', () => {
     fixture.detectChanges();
   }));
 
-  it('should create', () => {
+  it('should create the header component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a menu button in the header', () => {
+    const menuButton = fixture.debugElement.queryAll(By.css('ion-menu-button'));
+    expect(menuButton).toBeTruthy();
+  });
+
+  it('should have a centered title with an image', () => {
+    const title = fixture.debugElement.query(By.css('ion-title'));
+    expect(title).toBeTruthy();
+    const image = title.nativeElement.querySelector('img');
+    expect(image).toBeTruthy();
   });
 });
