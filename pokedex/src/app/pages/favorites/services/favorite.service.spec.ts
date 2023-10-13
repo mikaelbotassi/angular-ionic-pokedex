6c5d2a45248667a8af91bd13a87e59f041a5a84f
Mikael <elkamilive@gmail.com>
2023-10-12 21:59:25 -0300

    test: new test for favoriteservice working

--- pokedex/src/app/pages/favorites/services/favorite.service.spec.ts
+++ pokedex/src/app/pages/favorites/services/favorite.service.spec.ts
@@ -1,16 +1,51 @@
 import { TestBed } from '@angular/core/testing';
-
 import { FavoriteService } from './favorite.service';
 
 describe('FavoriteService', () => {
   let service: FavoriteService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers:[FavoriteService]
+    });
     service = TestBed.inject(FavoriteService);
+    localStorage.clear();
+  });
+
+  afterEach(() => {
+    localStorage.clear();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should add a favorite', () => {
+    const pokemonId = 1;
+
+    service.toggleFavorite(pokemonId);
+
+    service.favorites$.subscribe((favorites) => {
+      expect(favorites).toEqual([pokemonId]);
+    });
+
+    expect(service.isFavorite(pokemonId)).toBeTrue();
+  });
+
+  it('should remove a favorite', () => {
+    const pokemonId = 1;
+
+    service.toggleFavorite(pokemonId);
+
+    expect(service.isFavorite(pokemonId)).toBeTrue();
+
+    service.toggleFavorite(pokemonId);
+
+    service.favorites$.subscribe((favorites) => {
+      expect(favorites).toEqual([]);
+    });
+
+    expect(service.isFavorite(pokemonId)).toBeFalse();
+  });
+
 });
